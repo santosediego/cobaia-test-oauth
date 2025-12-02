5278b22692a4309e3016badb1f01d7300c09c0a4
santosediego <diegosantos.dev@outlook.com>
2025-12-02 11:30:57 -0300

    chore: initial project setup

---
+++ src/main/java/com/santosediego/cobaia_test/CobaiaTestApplication.java
@@ -0,0 +1,13 @@
+package com.santosediego.cobaia_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaiaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaiaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santosediego/cobaia_test/CobaiaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.cobaia_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaiaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
